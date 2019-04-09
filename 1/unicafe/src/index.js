19041753b7fee05a03788ec45f523dcc8475a41e
Dmitri Tsyganok <dmitri@tsyganok.net>
2019-04-10 01:16:14 +0300

    1.8: Part 1, unicafe Step 3

--- 1/unicafe/src/index.js
+++ 1/unicafe/src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom'
 
 const Button = (props) => (<button onClick={props.handleClick}>{props.text}</button>);
 
+const Statistics = ({good, neutral, bad}) => {
+  const sum = good+neutral+bad;
+  const avg = (good + bad*-1)/sum;
+  const positive = good/sum*100;
+
+  return (
+      <div>
+        <h2>statistiikka</h2>
+        <div>hyvä {good}</div>
+        <div>neutraali {neutral}</div>
+        <div>huono {bad}</div>
+        <div>yhteensä {sum}</div>
+        <div>keskiarvo {avg}</div>
+        <div>positiivisia {positive} %</div>
+      </div>
+  )
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -14,13 +32,7 @@ const App = () => {
         <Button handleClick={() => setGood(good+1)} text="Good"/>
         <Button handleClick={() => setNeutral(neutral+1)} text="Neutral"/>
         <Button handleClick={() => setBad(bad+1)} text="Bad"/>
-        <h2>statistiikka</h2>
-        <div>hyvä {good}</div>
-        <div>neutraali {neutral}</div>
-        <div>huono {bad}</div>
-        <div>yhteensä {good+neutral+bad}</div>
-        <div>keskiarvo {((good*1)+(neutral*0)+(bad*-1))/(good+neutral+bad)}</div>
-        <div>positiivisia {good/(good+neutral+bad)*100} %</div>
+        <Statistics good={good} neutral={neutral} bad={bad}/>
       </div>
   )
 };
