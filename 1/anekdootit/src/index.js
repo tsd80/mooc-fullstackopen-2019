70f706bc8129e612ce18e97f4b317028b66bf478
Dmitri Tsyganok <dmitri@tsyganok.net>
2019-04-10 11:19:37 +0300

    1.13: Part 1, anekdootit Step 2

--- 1/anekdootit/src/index.js
+++ 1/anekdootit/src/index.js
@@ -6,11 +6,20 @@ const Button = (props) => (<button onClick={props.handleClick}>{props.text}</but
 const App = (props) => {
 
   const [selected, setSelected] = useState(0);
+  const [score, setScore] = useState(Array.from(Array(props.anecdotes.length), ()=>0));
+
+  const handleScore = (point) => {
+    const newScore = [...score];
+    newScore[point] +=1;
+    setScore(newScore);
+  };
 
   return (
       <div>
         <div>{props.anecdotes[selected]}</div>
+        <div>has {score[selected]} points</div>
         <Button handleClick={() => setSelected(Math.floor(Math.random()*props.anecdotes.length))} text="next anecdote"/>
+        <Button handleClick={() => handleScore(selected)} text="Vote it!"/>
       </div>
   )
 };
@@ -21,7 +30,7 @@ const anecdotes = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ];
 
 ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
