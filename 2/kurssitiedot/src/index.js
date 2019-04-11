3be71be7fbdf3dce5172d589d4155e9aae23ee8b
Dmitri Tsyganok <dmitri@tsyganok.net>
2019-04-11 11:56:12 +0300

    2.5: Part 2, kurssitiedot erillinen moduuli

--- 2/kurssitiedot/src/index.js
+++ 2/kurssitiedot/src/index.js
@@ -1,41 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-
-const Course = (props) => {
-  return (
-      <div className="course">
-        <Header course={props.course.name}/>
-        <Content parts={props.course}/>
-        <Total total={props.course}/>
-      </div>
-  )
-};
-
-const Header = (props) => {
-  return (
-      <h1>{props.course}</h1>
-  )
-};
-
-const Content = (props) => {
-  const parts = () => props.parts.parts.map ((xx) => <Part key={xx.id} part={xx}/>);
-  return (<div className="content">{parts()}</div>)
-};
-
-const Part = (props) => {
-  return (
-      <p key={props.part.id}>{props.part.name} {props.part.exercises}</p>
-  )
-};
-
-const Total = (props) => {
-  let x= props.total.parts.reduce((acc, cur) => acc+cur.exercises,0);
-  return (
-      <div className="total"> yhteensä {x} tehtävää</div>
-  )
-};
-
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
