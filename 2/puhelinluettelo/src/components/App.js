8696df448539f8caa1fa00c589511ab3734ae801
Dmitri Tsyganok <dmitri@tsyganok.net>
2019-04-12 00:27:35 +0300

    2.8: Part 3, puhelinluettello

--- 2/puhelinluettelo/src/components/App.js
+++ 2/puhelinluettelo/src/components/App.js
@@ -8,39 +8,41 @@ const PrintOut = (props) => {
 
 const Name = (props) => {
   return (
-      <div>{props.name.name}</div>
+      <div>{props.name.name} {props.name.numero}</div>
   )
 };
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    {name: 'Arto Hellas' },
-    {name: 'Esko Ukkonen'}
+    {name: 'Arto Hellas', numero: '045-123456' },
+    {name: 'Esko Ukkonen', numero: '09-223345'}
   ]);
   const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
 
   const addPerson = (e) => {
+    console.log(e.target.name);
     e.preventDefault();
 
     if (persons.some((person) =>person.name===newName)) {window.alert(`${newName} on jo luettelossa`)}
     else {
-      const personObj = {name: newName};
+      const personObj = {name: newName, numero: newPhone};
       setPersons(persons.concat(personObj));
       setNewName('');
+      setNewPhone('');
     }
   };
 
   const onNameChange = (e) => setNewName(e.target.value);
-
+  const onPhoneChange = (e) => setNewPhone(e.target.value);
 
   return (
       <div>
 
         <h2>Puhelinluettelo</h2>
         <form onSubmit={addPerson}>
-          <div>
-            nimi: <input value={newName} onChange={onNameChange}/>
-          </div>
+          <div>nimi: <input value={newName} onChange={onNameChange} name="test"/></div>
+          <div>puhelinnumero: <input value={newPhone} onChange={onPhoneChange}/></div>
           <div>
             <button type="submit">lisää</button>
           </div>
